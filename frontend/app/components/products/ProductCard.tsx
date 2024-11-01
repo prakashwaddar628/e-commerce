a0493f47025fd890edaf6e0cd12c1aa1cb9f240d
prakashwaddar628 <prakashwaddar628@gmail.com>
2024-11-01 23:08:09 +0530

    new product router developed and product details page has been developing

--- frontend/app/components/products/ProductCard.tsx
+++ frontend/app/components/products/ProductCard.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import { formatPrice } from "@/utils/formatPrics";
+import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { format } from "path";
 
 interface ProductCardProps {
   data: any;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+
+  const router = useRouter();
+
+  const productRating =
+    data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+    data.reviews.length;
+
   return (
     <div
+    onClick={()=> router.push(`/product/${data.id}`)}
       className="col-span-1 cursor-pointer border-[1.2px]
         border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm"
     >
@@ -21,10 +34,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
             className="w-full h-full object-contain"
           />
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="mt-4 ">{truncateText(data.name)}</div>
+        <div>
+          <Rating value={productRating} readOnly />
+        </div>
+        <div>{data.reviews.length} reviews</div>
+        <div className="font-semibold ">{formatPrice(data.price)}</div>
       </div>
     </div>
   );
